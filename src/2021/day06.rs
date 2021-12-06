9039feefee54e4ec79756a36618008300e8b33c0
laurelkeys <tiagoloureirochaves@gmail.com>
2021-12-06 20:17:56 -0300

    Solve day 6

--- src/2021/day06.rs
+++ src/2021/day06.rs
@@ -6,22 +6,50 @@ use std::io::{self, BufRead, BufReader};
 /// https://adventofcode.com/2021/day/6
 pub struct Day06;
 
-impl Solver for Day06 {
-    type Input = Vec<Vec<i32>>;
-    type Output1 = i32;
-    type Output2 = i32;
+/// You can model each fish as a single number that represents the number of days until
+/// it creates a new lanternfish. Furthermore, you reason, a new lanternfish would surely
+/// need slightly longer before it's capable of producing more lanternfish: two more days
+/// for its first cycle.
+fn count_after(days: u64, ages: &[u8]) -> u64 {
+    let mut count = [0; 9];
+    for &age in ages {
+        count[age as usize] += 1;
+    }
+    for _ in 0..days {
+        count.rotate_left(1);
+        // Account for the "parents" which go back to 6
+        // (while the "new born" lanterfish start at 8).
+        count[6] += count[8];
+    }
+    count.iter().sum()
+}
 
-    ///
+impl Solver for Day06 {
+    type Input = Vec<u8>;
+    type Output1 = u64;
+    type Output2 = u64;
+
+    /// How many lanternfish would there be after 80 days?
     fn solve_part1(&self, input: &Self::Input) -> Self::Output1 {
-        todo!()
+        count_after(80, input)
     }
 
-    ///
+    /// How many lanternfish would there be after 256 days?
     fn solve_part2(&self, input: &Self::Input) -> Self::Output2 {
-        todo!()
+        count_after(256, input)
     }
 
     fn parse_input<R: io::Read>(&self, r: R) -> Self::Input {
-        todo!()
+        let input = BufReader::new(r)
+            .lines()
+            .flatten()
+            .flat_map(|line| {
+                line.split(',')
+                    .map(|age| age.parse().unwrap())
+                    .collect::<Vec<u8>>()
+            })
+            .collect();
+
+        input
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ fn solve_aoc_2021(day: u8) {
         3 => aoc2021::day03::Day03 {}.solve(Aoc2021, day), // 2583164, 2784375
         4 => aoc2021::day04::Day04 {}.solve(Aoc2021, day), // 58412, 10030
         5 => aoc2021::day05::Day05 {}.solve(Aoc2021, day), // 5124, 19771
-        6 => aoc2021::day06::Day06 {}.solve(Aoc2021, day), //
+        6 => aoc2021::day06::Day06 {}.solve(Aoc2021, day), // 349549, 1589590444365
         _ => eprintln!("Day {} hasn't been solved yet 😅", day),
     }
 }
